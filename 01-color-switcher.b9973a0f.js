59d193e468ead6e85a0f812899d031e1e4205d45
VitaliiCha <110927499+VitaliiCha@users.noreply.github.com>
2023-01-27 02:18:08 +0000

    Deploying to gh-pages from @ VitaliiCha/goit-js-hw-09@e33864e97170e459a7482fefe0a80a99414c6015 🚀

---
+++ 01-color-switcher.b9973a0f.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=null;t.startBtn.addEventListener("click",(()=>{t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.b9973a0f.js.map
